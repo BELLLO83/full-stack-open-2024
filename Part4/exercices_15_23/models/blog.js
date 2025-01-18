8c7c2683b9fe86f1feaeeefea92b9801b8d7322f
BELLAJ Mohamed <mohamed.bellaj@etu.uae.ac.ma>
2025-01-18 22:20:13 +0100

    Add files via upload

---
+++ Part4/exercices_15_23/models/blog.js
@@ -0,0 +1,72 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// mongoose.set("strictQuery", false);
+
+// const url = process.env.MONGODB_URI;
+
+// console.log("connecting to", url);
+
+// mongoose
+//   .connect(url)
+
+//   .then((result) => {
+//     console.log("connected to MongoDB");
+//   })
+//   .catch((error) => {
+//     console.log("error connecting to MongoDB:", error.message);
+//   });
+
+// const personSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     minLength: 3,
+//     required: true,
+//   },
+//   number: {
+//     type: String,
+//     required: true,
+//     validate: {
+//       validator: function (v) {
+//         return /\d{2,3}-\d+$/.test(v) && v.length >= 8;
+//       },
+//       message: (props) => `${props.value} is not a valid phone number!`,
+//     },
+//   },
+// });
+
+// personSchema.set("toJSON", {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString();
+//     delete returnedObject._id;
+//     delete returnedObject.__v;
+//   },
+// });
+
+// module.exports = mongoose.model("Person", personSchema);
+
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
